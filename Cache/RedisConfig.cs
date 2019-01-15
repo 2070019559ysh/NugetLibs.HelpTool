0815d4191c822322d4ecd904bfbf5da219b36e78
YSH <yangshanghong@zs-zc.net>
2019-01-15 18:02:43 +0800

    添加基本加解密、IP、Web请求、Redis操作，并初始化项目

---
+++ Cache/RedisConfig.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NugetLibs.HelpTool.Cache
+{
+    /// <summary>
+    /// Redis的配置信息
+    /// </summary>
+    public class RedisConfig
+    {
+        /// <summary>
+        /// 配置系统RedisKey的前缀（使用:来分割前缀，如Demo:test: 可视化工具查看的时候就比较好按目录区分）
+        /// </summary>
+        public string SysCustomPrefix { get; set; } = "DefaultPrefix:";
+        /// <summary>
+        /// Redis的连接字符串
+        /// </summary>
+        public string RedisConnect { get; set; } = "localhost";
+    }
+}
